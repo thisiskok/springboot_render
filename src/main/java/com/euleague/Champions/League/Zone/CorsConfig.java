d3f9c43c8cf75665b55e3568cea29e91fbbb2ce8
yongjie hao <139759860+thisiskok@users.noreply.github.com>
2025-02-21 16:11:41 +0100

    check backend cant connect database root cause

--- src/main/java/com/euleague/Champions/League/Zone/CorsConfig.java
+++ src/main/java/com/euleague/Champions/League/Zone/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
       @Override
       public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000", "https://champions-league-fronted.vercel.app")
+            .allowedOrigins("*", "https://champions-league-fronted.vercel.app")
             .allowedMethods("GET");
       }
     };
